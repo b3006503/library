84e8db568748d5022c0e6dbcdfbec7b13f36ed23
adamclow <amcstoke@gmail.com>
2015-03-16 12:36:32 +0000

    Creating relationships for all classes

--- library/grails-app/domain/com/library/Course.groovy
+++ library/grails-app/domain/com/library/Course.groovy
@@ -8,6 +8,12 @@ String tutor
 String department
 String description
 
+
+String toString(){
+
+"$title, $code, $department"
+}
+
     static constraints = {
 title()
 code()
--- library/grails-app/domain/com/library/Student.groovy
+++ library/grails-app/domain/com/library/Student.groovy
@@ -5,7 +5,15 @@ class Student {
 String name
 String email
 String studentID
-String course
+Course course
+
+
+static hasMany =[books:Book]
+
+String toString(){
+"$name, $email"
+
+}
 
     static constraints = {
 
