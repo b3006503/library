12c70b283c2ae482ce6606a6cf2b4fb86651d79e
adamclow <amcstoke@gmail.com>
2015-03-16 11:10:42 +0000

    Student class done plus scaffold

---
+++ library/grails-app/domain/com/library/Student.groovy
@@ -0,0 +1,17 @@
+package com.library
+
+class Student {
+
+String name
+String email
+String studentID
+String course
+
+    static constraints = {
+
+name()
+email()
+studentID()
+course()
+    }
+}
